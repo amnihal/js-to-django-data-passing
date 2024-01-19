ed7bf3fd5a2d132bb9823a8664e06614b018e1ca
Nihal aduvarakkal <72524779+amnihal@users.noreply.github.com>
2024-01-19 08:08:04 +0530

    first commit

---
+++ qrpz/home/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+
+
+
+
+def scanner(request):
+
+    qr_data = request.POST.get('qr')
+    print('Received data:', qr_data)
+
+    return JsonResponse({'status': 'success'})
+
+def home(request):
+    
+    return render(request,'scanner.html')
